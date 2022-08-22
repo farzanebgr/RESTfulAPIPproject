6045bf99c8978757c0da72b26cee8a16f7a3a8bf
farzanebgr <farzenehboogari@gmail.com>
2022-08-22 16:59:45 +0430

    Nested relationships

--- watch_list_app/api/serializers.py
+++ watch_list_app/api/serializers.py
@@ -19,6 +19,8 @@ class WatchListSerializer(serializers.ModelSerializer):
 
 
 class StreamPlatformSerializer(serializers.ModelSerializer):
+    watchlist = WatchListSerializer(many=True, read_only=True)
+
     class Meta:
         model = StreamPlatform
         fields = "__all__"
