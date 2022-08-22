cb43e71a1bbe68c6567c66ba84bf44cb81bcd50d
farzanebgr <farzenehboogari@gmail.com>
2022-08-22 16:43:48 +0430

    add relation field to watchlist model

--- watch_list_app/models.py
+++ watch_list_app/models.py
@@ -1,16 +1,6 @@
 from django.db import models
 
 
-class WatchList(models.Model):
-    title = models.CharField(max_length=50)
-    storyline = models.CharField(max_length=200)
-    active = models.BooleanField(default=True)
-    created = models.DateTimeField(auto_now_add=True)
-
-    def __str__(self):
-        return self.title
-
-
 class StreamPlatform(models.Model):
     name = models.CharField(max_length=50)
     about = models.CharField(max_length=150)
@@ -19,6 +9,16 @@ class StreamPlatform(models.Model):
     def __str__(self):
         return self.name
 
+
+class WatchList(models.Model):
+    title = models.CharField(max_length=50)
+    storyline = models.CharField(max_length=200)
+    platform = models.ForeignKey(StreamPlatform, on_delete=models.CASCADE, related_name="watchlist")
+    active = models.BooleanField(default=True)
+    created = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
 
 # Create your models here.
 # class Movie(models.Model):
